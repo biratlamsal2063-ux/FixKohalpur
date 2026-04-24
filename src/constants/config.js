2f58b98ae0a5806574bd97146d8f2cbd7797a4b1
Birat Lamsal <biratlamsal2063@gmail.com>
2026-04-24 19:13:39 +0545

    e sewa integrated

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,8 +1,8 @@
 export const CONFIG = {
     ESEWA_MERCHANT_CODE: 'EPAYTEST',          // Replace with live code
-    ESEWA_SUCCESS_URL: 'https://fixkohalpur.com/payment/success',
-    ESEWA_FAILURE_URL: 'https://fixkohalpur.com/payment/failure',
-    ESEWA_PAYMENT_URL: 'https://uat.esewa.com.np/epay/main', // UAT (test)
+    ESEWA_SUCCESS_URL: 'https://developer.esewa.com.np/success',
+    ESEWA_FAILURE_URL: 'https://developer.esewa.com.np/failure',
+    ESEWA_PAYMENT_URL: 'https://esewa.com.np/#/home', // UAT (test)
     // Live: 'https://esewa.com.np/epay/main'
     APP_NAME: 'Fix Kohalpur',
     SUPPORT_PHONE: '+977-9XXXXXXXXX',
